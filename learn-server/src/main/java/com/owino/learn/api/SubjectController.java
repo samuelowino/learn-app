70f7881b32a96f3ed1198d84bb75e0db2b274c54
samuelowino <samuelowino43@yahoo.com>
2022-11-24 15:09:36 +0300

    Subjects api

--- learn-server/src/main/java/com/owino/learn/api/SubjectController.java
+++ learn-server/src/main/java/com/owino/learn/api/SubjectController.java
@@ -2,7 +2,9 @@ package com.owino.learn.api;
 
 import com.owino.learn.api.resource.SubjectResource;
 import com.owino.learn.services.SubjectService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,11 @@ public class SubjectController {
     @PostMapping("/")
     public void createSubject(@RequestBody SubjectResource resource){
         subjectService.create(resource);
+    }
+
+    @GetMapping("/")
+    public List<SubjectResource> getAllSubjects(){
+        return subjectService.findAll();
     }
 
 }
--- learn-server/src/test/java/com/owino/learn/rest_api_tests/SubjectControllerTest.java
+++ learn-server/src/test/java/com/owino/learn/rest_api_tests/SubjectControllerTest.java
@@ -3,28 +3,29 @@ package com.owino.learn.rest_api_tests;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.owino.learn.api.SubjectController;
 import com.owino.learn.api.resource.SubjectResource;
-import com.owino.learn.config.WebConfig;
 import com.owino.learn.entities.Subject;
 import com.owino.learn.services.SubjectService;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {WebConfig.class})
 @Import(SubjectController.class)
-@WebMvcTest(SubjectControllerTest.class)
+@WebMvcTest(SubjectController.class)
 public class SubjectControllerTest {
 
     @Autowired
@@ -54,9 +55,25 @@ public class SubjectControllerTest {
                 .andReturn();
     }
 
-//    @Test
-//    public void shouldFetchAllSubjectsEndPointTest() throws Exception {
-//        this.mockMvc.perform(get("/subject/"))
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    public void shouldFetchAllSubjectsEndPointTest() throws Exception {
+        this.mockMvc.perform(get("/subject/"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void shouldReturnListOfSubjectsTest() throws Exception {
+        Mockito.when(subjectService.findAll())
+                .thenReturn(Arrays.asList( new SubjectResource(1L, "Physics")));
+
+        this.mockMvc.perform(get("/subject/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("@.[0].id").isNumber())
+                .andExpect(jsonPath("@.[0].id").value(1L))
+                .andExpect(jsonPath("@.[0].name").value("Physics"))
+                .andDo(print())
+                .andReturn();
+    }
 }
